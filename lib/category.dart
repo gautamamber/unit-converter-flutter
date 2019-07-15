b3ae3343913314e8785defa8105bab04ac2de2f2
gautamamber <ambergautam1@gmail.com>
2019-07-16 00:58:20 +0530

    feat(Add Units navigator)

--- lib/category.dart
+++ lib/category.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
-
+import 'converter_route.dart';
 // @ required is defined in meta package
 import "package:meta/meta.dart";
-
+import 'unit.dart';
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight/2);
 
@@ -10,15 +10,42 @@ class Category extends StatelessWidget {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
+  final List<Unit> units;
   const Category({
     Key key,
     @required this.name,
     @required this.color,
-    @required this.iconLocation
+    @required this.iconLocation,
+    @required this.units,
   }) :assert(name!= null),
   assert(color!= null),
   assert(iconLocation != null),
+  assert(units!=null),
   super(key: key);
+
+
+  void _navigateToConverter(BuildContext context){
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (BuildContext context){
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name, style: Theme.of(context).textTheme.display1,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            name: name,
+            units: units,
+            ),
+        );
+      }
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -30,9 +57,7 @@ class Category extends StatelessWidget {
           highlightColor: color,
           splashColor: color,
 
-          onTap: (){
-            print("I was tapped");
-          },
+          onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
